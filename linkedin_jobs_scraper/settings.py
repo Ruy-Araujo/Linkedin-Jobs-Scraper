a7f94470e0bc1e03dc020fbbfff425505049f546
Ruy Araujo <raraujo@vibra.digital>
2023-02-18 01:58:27 -0300

    feat: Jobs spider

--- linkedin_jobs_scraper/settings.py
+++ linkedin_jobs_scraper/settings.py
@@ -25,9 +25,9 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-# DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 0.25
 # The download delay setting will honor only one of:
-# CONCURRENT_REQUESTS_PER_DOMAIN = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 10
 # CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
