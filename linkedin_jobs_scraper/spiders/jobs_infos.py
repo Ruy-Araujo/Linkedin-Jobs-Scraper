28a2f2f6240ddab0116a71999b68aa0eac0072e7
Ruy Araujo <raraujo@vibra.digital>
2023-04-28 00:04:11 -0300

    Add job_id to each scraped job and enable spider to keep scraping jobs until exhausted
    - Add a __repr__ method to JobsInfosItem to return the scraped job_id.
    - Enable the JobsInfosScraper spider to keep scraping by adding job_id attribute and a __next__ method to return the next job_id in line. Stop spider when there are no more jobs left to scrape by raising a CloseSpider exception.

--- linkedin_jobs_scraper/spiders/jobs_infos.py
+++ linkedin_jobs_scraper/spiders/jobs_infos.py
@@ -3,6 +3,7 @@ import time
 
 import scrapy
 from scrapy.loader import ItemLoader
+from scrapy.exceptions import CloseSpider
 from scrapy.spidermiddlewares.httperror import HttpError
 from scrapy.utils.project import get_project_settings
 from scrapy.utils.response import response_status_message
@@ -21,13 +22,13 @@ class JobsInfosScraper(scrapy.Spider):
     url = "https://www.linkedin.com/voyager/api/jobs/jobPostings/"
 
     def start_requests(self):
+        self.job_id = iter(self.jobs_ids)
         self.cookies = parse_load(self.settings.get("LINKEDIN_COOKIES"))
         self.headers = {
             "csrf-token": self.settings.get("CSRF_TOKEN"),
         }
 
-        for id in self.jobs_ids:
-            yield scrapy.Request(url=self.url + id, callback=self.parse, cookies=self.cookies, headers=self.headers)
+        yield scrapy.Request(url=self.url + next(self.job_id), callback=self.parse, cookies=self.cookies, headers=self.headers)
 
     def parse(self, response):
         json_response = response.json()
@@ -37,3 +38,10 @@ class JobsInfosScraper(scrapy.Spider):
         item.add_value("data", json_response)
         item.add_value("extracted_at", datetime.now().isoformat())
         yield item.load_item()
+
+        next_job = next(self.job_id, None)
+
+        if not next_job:
+            raise CloseSpider("No more results, scrapy stoped...")
+
+        yield response.follow(url=self.url + next_job, callback=self.parse, cookies=self.cookies, headers=self.headers)
